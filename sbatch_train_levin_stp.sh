c60957c8e7aadbece8577e797955c2aea5395321
levilelis <levilelis@gmail.com>
2020-05-21 20:59:03 -0600

    changing names of scripts

--- sbatch_train_levin_stp.sh
+++ sbatch_train_levin_stp.sh
@@ -8,6 +8,7 @@ heuristic_scheme=("--learned-heuristic --default-heuristic" "--default-heuristic
 algorithm="Levin"
 
 for iter in {1..1}; do
+echo ${iter}
 	for scheme in "${heuristic_scheme[@]}"; do
 		for loss in ${losses[@]}; do
 			lower_loss=$(echo ${loss} | tr "A-Z" "a-z")
@@ -39,40 +40,5 @@ for iter in {1..1}; do
 
 			sbatch --output=${output_exp} --export=scheme=${scheme},algorithm=${algorithm},loss=${loss},model=${model} run_bootstrap_train.sh
 		done
-	done
-done
-
-heuristic_scheme=("--learned-heuristic --default-heuristic" "--learned-heuristic") 
-algorithm="GBFS"
-
-for iter in {1..1}; do
-	for scheme in "${heuristic_scheme[@]}"; do
-		lower_loss=$(echo ${loss} | tr "A-Z" "a-z")
-		lower_algorithm=$(echo ${algorithm} | tr "A-Z" "a-z")
-		name_scheme=${scheme// /}
-		name_scheme=${name_scheme//-heuristic/}
-		name_scheme=${name_scheme//--/-}
-		output_exp="${output}${lower_algorithm}${name_scheme}-v${iter}"
-		model=${domain_name}${lower_algorithm}${name_scheme}-v${iter}
-
-		sbatch --output=${output_exp} --export=scheme=${scheme},algorithm=${algorithm},model=${model} run_bootstrap_train.sh
-
-	done
-done
-
-heuristic_scheme=("--learned-heuristic --default-heuristic" "--learned-heuristic") 
-algorithm="AStar"
-
-for iter in {1..1}; do
-	for scheme in "${heuristic_scheme[@]}"; do
-		lower_loss=$(echo ${loss} | tr "A-Z" "a-z")
-		lower_algorithm=$(echo ${algorithm} | tr "A-Z" "a-z")
-		name_scheme=${scheme// /}
-		name_scheme=${name_scheme//-heuristic/}
-		name_scheme=${name_scheme//--/-}
-		output_exp="${output}${lower_algorithm}${name_scheme}-v${iter}"
-		model=${domain_name}${lower_algorithm}${name_scheme}-v${iter}
-
-		sbatch --output=${output_exp} --export=scheme=${scheme},algorithm=${algorithm},model=${model} run_bootstrap_train.sh
 	done
 done
