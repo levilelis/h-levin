3419401f94ad95f080c82b22276f147df0626726
levilelis <levilelis@gmail.com>
2020-06-01 21:31:01 -0600

    adjusting scripts to run in 2 days

--- run_bootstrap_test_sokoban.sh
+++ run_bootstrap_test_sokoban.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=1   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12000M        # memory per node
-#SBATCH --time=0-23:59      # time (DD-HH:MM)
+#SBATCH --time=2-0:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --account=def-lelis
 
 module load python/3.6
 source tensorflow/bin/activate
-python src/main.py ${scheme} -a ${algorithm} -m ${model} -p ${problem} -b 2000 -time 86400 -d Sokoban
+python src/main.py ${scheme} -a ${algorithm} -m ${model} -p ${problem} -b 2000 -time 172200 -d Sokoban
