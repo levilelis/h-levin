cda39bdad144dc20e5269e9ef63dd7bdea38a332
levilelis <levilelis@gmail.com>
2020-08-03 19:35:41 -0600

    removing unused scripts

--- sbatch_bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sbatch --export=problems=puzzles_4x4,loss=LevinLoss,model_name=4x4_levin_learned_h,use_h=y,use_learned_h=y run_bootstrap.sh
-sbatch --export=problems=puzzles_4x4,loss=CrossEntropyLoss,model_name=4x4_cross_learned_h,use_h=y,use_learned_h=y run_bootstrap.sh
-
-sbatch --export=problems=puzzles_4x4,loss=LevinLoss,model_name=4x4_levin_h,use_h=y,use_learned_h=f run_bootstrap.sh
-sbatch --export=problems=puzzles_4x4,loss=CrossEntropyLoss,model_name=4x4_cross_h,use_h=y,use_learned_h=f run_bootstrap.sh
