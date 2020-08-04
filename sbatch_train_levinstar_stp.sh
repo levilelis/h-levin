97c439aab7b3bdcfdcce9a02cd63c135fbf7ce79
levilelis <levilelis@gmail.com>
2020-08-04 09:35:34 -0600

    adding scripts for running stp experiments

--- sbatch_train_levinstar_stp.sh
+++ sbatch_train_levinstar_stp.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-declare -a losses=("RegLevinLoss")
-#declare -a losses=("CrossEntropyLoss" "ImprovedLevinLoss" "LevinLoss")
+#declare -a losses=("RegLevinLoss")
+declare -a losses=("CrossEntropyLoss" "ImprovedLevinLoss" "LevinLoss")
 output="output_train_stp/"
-domain_name="4x4-stp-"
+domain_name="6x6-stp-"
 algorithm="LevinStar"
 
-heuristic_scheme=("--learned-heuristic --default-heuristic" "--default-heuristic" "--learned-heuristic")
+#heuristic_scheme=("--learned-heuristic --default-heuristic" "--default-heuristic" "--learned-heuristic")
+heuristic_scheme=("--learned-heuristic")
 
-for iter in {1..5}; do
+scheduler="online"
+
+for iter in {1..1}; do
 	for scheme in "${heuristic_scheme[@]}"; do
 		for loss in ${losses[@]}; do
 			lower_loss=$(echo ${loss} | tr "A-Z" "a-z")
@@ -16,10 +19,10 @@ for iter in {1..5}; do
 			name_scheme=${scheme// /}
 			name_scheme=${name_scheme//-heuristic/}
 			name_scheme=${name_scheme//--/-}
-			output_exp="${output}${lower_algorithm}-${lower_loss}${name_scheme}-v${iter}"
-			model=${domain_name}${lower_algorithm}-${lower_loss}${name_scheme}-v${iter}
+			output_exp="${output}${lower_algorithm}-${lower_loss}${name_scheme}-${scheduler}-v${iter}"
+			model=${domain_name}${lower_algorithm}-${lower_loss}${name_scheme}-${scheduler}-v${iter}
 
-			sbatch --output=${output_exp} --export=scheme="${scheme}",algorithm=${algorithm},loss=${loss},model=${model} run_bootstrap_train.sh
+			sbatch --output=${output_exp} --export=scheme="${scheme}",algorithm=${algorithm},loss=${loss},model=${model},scheduler=${scheduler} run_bootstrap_train_stp.sh
 		done
 	done
 done
